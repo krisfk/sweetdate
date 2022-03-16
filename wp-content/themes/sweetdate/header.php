20ba457345859fa68a76f4988deced356fb49a52
kaiyu li <krisfk@gmail.com>
2022-03-17 03:07:41 +0800

    .

--- wp-content/themes/sweetdate/header.php
+++ wp-content/themes/sweetdate/header.php
@@ -124,7 +124,7 @@ echo'</li>';
 
 ?>
 
-                    <li>
+                    <li class="m-0">
                         <a class="sns-a" href="javascript:void()">
                             <img src="<?php echo get_template_directory_uri()?>/assets/images/ig-icon.png" alt="">
                         </a>
