1a13ded270c37879043ca33cc04639c4b5051c72
kaiyu li <krisfk@gmail.com>
2022-03-17 03:45:09 +0800

    .

--- wp-content/themes/sweetdate/page-home.php
+++ wp-content/themes/sweetdate/page-home.php
@@ -65,13 +65,13 @@ get_header();
 
         <div class="col-6">
 
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col-6">
                     <h2 class="d-inline-block">
                         <img src="<?php echo get_template_directory_uri()?>/assets/images/icon-1.png" alt=""> 關於蜜遇
                     </h2>
                 </div>
-                <div class="col-6">
+                <div class="col-6 text-end">
                     <a href="#" class="more-btn">詳細簡介 〉</a>
                 </div>
             </div>
