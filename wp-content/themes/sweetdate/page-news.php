4f209db9fa1097aa804e8e327f8dffbd8fb7dcf1
kaiyu li <krisfk@gmail.com>
2022-03-17 17:30:22 +0800

    .

--- wp-content/themes/sweetdate/page-news.php
+++ wp-content/themes/sweetdate/page-news.php
@@ -23,7 +23,7 @@ get_header();
 
         <ul class="filter-ul">
             <li>
-                <a href="active">顯示全部</a>
+                <a class="active" href="#">顯示全部</a>
             </li>
             <li>
                 <a href="">約會資訊</a>
