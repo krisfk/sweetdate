bff59e90a0af415df1ef74f89b883c73a2786923
kaiyu li <krisfk@gmail.com>
2022-03-17 16:23:08 +0800

    .

--- wp-content/themes/sweetdate/page-home.php
+++ wp-content/themes/sweetdate/page-home.php
@@ -218,13 +218,13 @@ get_header();
                         <div class="gender-div male">
                             <table>
                                 <tr>
-                                    <td>
+                                    <td class="p-5">
                                         <img src="<?php echo get_template_directory_uri()?>/assets/images/m-icon.png"
                                             alt="">
 
 
                                     </td>
-                                    <td>
+                                    <td class="p-5">
                                         <div class="row">
 
                                             <div class="col-4">名額 : 10人 (36 ~ 41歲)</div>
