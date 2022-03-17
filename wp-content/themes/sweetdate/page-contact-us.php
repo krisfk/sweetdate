eb8247684b6a491cf03944abda05c5a6b4786658
kaiyu li <krisfk@gmail.com>
2022-03-17 17:46:49 +0800

    .

---
+++ wp-content/themes/sweetdate/page-contact-us.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+<div class="container mt-3">
+
+    <h2 class="title-type-pink-line">關於我們</h2>
+
+
+    <div class="main-content mt-4">
+
+        <div class="text-center">
+            <img src="http://64.227.13.14/sweetdate/wp-content/uploads/2022/03/s-foto-1.jpg" alt="">
+        </div>
+        <br>
+        <div>
+            為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，
+        </div>
+        <br> <br>
+
+        <div class="text-center">
+            <img src="http://64.227.13.14/sweetdate/wp-content/uploads/2022/03/s-foto-1.jpg" alt="">
+        </div>
+        <br>
+        <div>
+            為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，為你成就緣份的專業配對團隊。尋找你人生伴侶。高端約會，
+        </div>
+
+
+
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+<?php
+/* Start the Loop */
+// while ( have_posts() ) :
+// 	the_post();
+// 	get_template_part( 'template-parts/content/content-page' );
+
+// 	// If comments are open or there is at least one comment, load up the comment template.
+// 	if ( comments_open() || get_comments_number() ) {
+// 		comments_template();
+// 	}
+// endwhile; // End of the loop.
+
+get_footer();
