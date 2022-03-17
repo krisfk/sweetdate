3cd673bd7726e4bb85f81db49cf668fa516a83e8
kaiyu li <krisfk@gmail.com>
2022-03-17 18:19:12 +0800

    .

--- wp-content/themes/sweetdate/page-home.php
+++ wp-content/themes/sweetdate/page-home.php
@@ -120,7 +120,7 @@ get_header();
 
         <div class="position-relative">
             <h2 class="title-type-pink-line">成功故事</h2>
-            <a href="#" class="more-btn position-absolute top-0 end-0">全部故事 〉</a>
+            <!-- <a href="#" class="more-btn position-absolute top-0 end-0">全部故事 〉</a> -->
 
         </div>
 
