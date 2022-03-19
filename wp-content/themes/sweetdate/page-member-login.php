0cfa448f3dc7c37200b3d0c995a575bbfb2c0f72
kaiyu li <krisfk@gmail.com>
2022-03-19 22:47:14 +0800

    .

--- wp-content/themes/sweetdate/page-member-login.php
+++ wp-content/themes/sweetdate/page-member-login.php
@@ -32,7 +32,7 @@ get_header();
                 </tr>
                 <tr>
                     <td></td>
-                    <td><input type="submit" class="form-submit-btn" value="登入"></td>
+                    <td class="text-end"><input type="submit" class="form-submit-btn" value="登入"></td>
                 </tr>
             </table>
         </form>
