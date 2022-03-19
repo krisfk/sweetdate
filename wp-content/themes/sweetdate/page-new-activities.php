3b4b9bf19e7f8386fc559350108bfefa500e773d
kaiyu li <krisfk@gmail.com>
2022-03-19 23:09:18 +0800

    .

--- wp-content/themes/sweetdate/page-new-activities.php
+++ wp-content/themes/sweetdate/page-new-activities.php
@@ -29,7 +29,111 @@ get_header();
 for($i=0;$i<6;$i++)
 {
     ?>
-            <div class="col-4 mb-4 position-relative">
+            <div class="col-6 mb-2">
+
+                <a href="#" class="s-case-div-a"><img
+                        src="http://64.227.13.14/sweetdate/wp-content/uploads/2022/03/s-foto-1.jpg" alt="">
+                    <div class="mt-4">
+
+
+                        <h2>成熟人士. 婚活族New Year Wish下午茶</h2>
+
+
+
+                        <div>
+                            <ul class="news-act-info-ul">
+                                <li>
+                                    <div class="d-table">
+                                        <div class="d-table-cell pe-1">
+                                            <img src="http://64.227.13.14/sweetdate/wp-content/uploads/2022/03/small-cal-icon.png"
+                                                alt="">
+                                        </div>
+                                        <div class="d-table-cell">
+                                            12月9日</div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="d-table">
+                                        <div class="d-table-cell pe-1">
+                                            <img src="http://64.227.13.14/sweetdate/wp-content/uploads/2022/03/small-clock-icon.png"
+                                                alt="">
+                                        </div>
+                                        <div class="d-table-cell">
+                                            15:30 - 17:00</div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="d-table">
+                                        <div class="d-table-cell pe-1">
+                                            <img src="http://64.227.13.14/sweetdate/wp-content/uploads/2022/03/pin-icon.png"
+                                                alt="">
+                                        </div>
+                                        <div class="d-table-cell">
+                                            上環西餐廳 (鄰近MTR)</div>
+                                    </div>
+                                </li>
+                            </ul>
+
+
+
+                        </div>
+
+                        <div class="mt-3">來結織同樣希望以組織家庭為前題的對象! <br>
+                            飲品及精美下午茶 <br>
+                            創配對遊戲, 助你極速了解對象, 送精美奬品 <br>
+                            一對一論流傾談時段 (每組4-6分鐘) <br>
+                            使用Dating自動配對系統 (全電腦操作// 保證100% )</div>
+
+                        <div class="gender-div male">
+                            <table>
+                                <tr>
+                                    <td class="gender-icon-td">
+                                        <img src="<?php echo get_template_directory_uri()?>/assets/images/m-icon.png"
+                                            alt="">
+
+
+                                    </td>
+                                    <td class="">
+                                        <div class="row">
+
+                                            <div class="col-5">名額 : 10人 (36 ~ 41歲)</div>
+                                            <div class="col-7">餘額 : 2人</div>
+                                            <div class="col-12 mt-1">大學程度或以上, 穏定職業及希望組織家庭</div>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            </table>
+                        </div>
+
+
+                        <div class="gender-div female">
+                            <table>
+                                <tr>
+                                    <td class="gender-icon-td">
+                                        <img src="<?php echo get_template_directory_uri()?>/assets/images/f-icon.png"
+                                            alt="">
+
+
+                                    </td>
+                                    <td class="">
+                                        <div class="row">
+
+                                            <div class="col-5">名額 : 10人 (36 ~ 41歲)</div>
+                                            <div class="col-7">餘額 : 2人</div>
+                                            <div class="col-12 mt-1">大學程度或以上, 穏定職業及希望組織家庭</div>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            </table>
+                        </div>
+
+
+                    </div>
+                </a>
+            </div>
+            <!-- <div class="col-4 mb-4 position-relative">
 
                 <div class="news-label">約會資訊</div>
                 <a href="#" class="s-case-div-a"><img
@@ -54,7 +158,7 @@ for($i=0;$i<6;$i++)
 
                     </div>
                 </a>
-            </div>
+            </div> -->
             <?php
 }
 ?>
