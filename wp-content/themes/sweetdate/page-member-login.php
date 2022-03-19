83c70e35cee59f8fc8ea5ecee55360830885c5d0
kaiyu li <krisfk@gmail.com>
2022-03-19 22:39:17 +0800

    .

---
+++ wp-content/themes/sweetdate/page-member-login.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+<div class="container mt-3">
+
+    <h2 class="title-type-pink-line">會員登入</h2>
+
+
+    <div class="main-content mt-4">
+
+        fds
+
+
+
+
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+</div>
+<script type="text/javascript">
+$(function() {
+
+    $('.qt').click(function() {
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $(this).next('.ans').slideDown(200);
+        } else {
+            $(this).next('.ans').slideUp(200);
+
+        }
+    });
+})
+</script>
+
+
+<?php
+/* Start the Loop */
+// while ( have_posts() ) :
+// 	the_post();
+// 	get_template_part( 'template-parts/content/content-page' );
+
+// 	// If comments are open or there is at least one comment, load up the comment template.
+// 	if ( comments_open() || get_comments_number() ) {
+// 		comments_template();
+// 	}
+// endwhile; // End of the loop.
+
+get_footer();
