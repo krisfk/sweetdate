b89526a0b2ca8ad4cb5d3248a86b2d8979c1c23f
kaiyu li <krisfk@gmail.com>
2022-03-19 22:46:36 +0800

    .

--- wp-content/themes/sweetdate/page-member-login.php
+++ wp-content/themes/sweetdate/page-member-login.php
@@ -20,21 +20,22 @@ get_header();
     <div class="main-content mt-4">
 
 
-        <table>
-            <tr>
-                <td><label for="login-email"> 登記電郵</label></td>
-                <td><input type="text" class="form-control" id="login-email" name="login-email"></td>
-            </tr>
-            <tr>
-                <td><label for="login-password">登入密碼</label> </td>
-                <td><input type="text" class="form-control" id="login-password" name="login-password"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" class="form-submit-btn" value="登入"></td>
-            </tr>
-        </table>
-
+        <form action="" class="login-form">
+            <table>
+                <tr>
+                    <td><label for="login-email"> 登記電郵</label></td>
+                    <td><input type="text" class="form-control" id="login-email" name="login-email"></td>
+                </tr>
+                <tr>
+                    <td><label for="login-password">登入密碼</label> </td>
+                    <td><input type="text" class="form-control" id="login-password" name="login-password"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" class="form-submit-btn" value="登入"></td>
+                </tr>
+            </table>
+        </form>
 
 
 
