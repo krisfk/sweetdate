eecf915dc535b6291b712f3a4add131db4d8ce59
kaiyu li <krisfk@gmail.com>
2022-03-19 22:49:08 +0800

    .

---
+++ wp-content/themes/sweetdate/page-member-register.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+<div class="container mt-3">
+
+    <h2 class="title-type-pink-line">會員註冊</h2>
+
+
+    <div class="main-content mt-4">
+
+
+        <form action="" class="login-form">
+            <table>
+                <tr>
+                    <td><label for="login-email"> 登記電郵</label></td>
+                    <td><input type="text" class="form-control" id="login-email" name="login-email"></td>
+                </tr>
+                <tr>
+                    <td><label for="login-password">登入密碼</label> </td>
+                    <td><input type="text" class="form-control" id="login-password" name="login-password"></td>
+                </tr>
+                <tr>
+                    <td><label for="login-password-again">再次輸入登入密碼</label> </td>
+                    <td><input type="text" class="form-control" id="login-password" name="login-password-again"></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td class="text-end"><input type="submit" class="form-submit-btn" value="登入"></td>
+                </tr>
+            </table>
+        </form>
+
+
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+</div>
+<script type="text/javascript">
+$(function() {
+
+    $('.qt').click(function() {
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $(this).next('.ans').slideDown(200);
+        } else {
+            $(this).next('.ans').slideUp(200);
+
+        }
+    });
+})
+</script>
+
+
+<?php
+/* Start the Loop */
+// while ( have_posts() ) :
+// 	the_post();
+// 	get_template_part( 'template-parts/content/content-page' );
+
+// 	// If comments are open or there is at least one comment, load up the comment template.
+// 	if ( comments_open() || get_comments_number() ) {
+// 		comments_template();
+// 	}
+// endwhile; // End of the loop.
+
+get_footer();
