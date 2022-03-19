83fe7a77857fadddbfd89b745e994bb429622cd5
kaiyu li <krisfk@gmail.com>
2022-03-19 22:55:39 +0800

    .

--- wp-content/themes/sweetdate/page-member-register.php
+++ wp-content/themes/sweetdate/page-member-register.php
@@ -39,8 +39,8 @@ get_header();
                 <tr>
                     <td>姓別</td>
                     <td>
-                        <label class="pe-2"><input class="pe-1" type="radio" name="gender" value="男">男</label>
-                        <label class="pe-2"><input class="pe-1" type="radio" name="gender" value="女">女</label>
+                        <label class="me-2"><input class="me-1" type="radio" name="gender" value="男">男</label>
+                        <label class="me-2"><input class="me-1" type="radio" name="gender" value="女">女</label>
                     </td>
                 </tr>
 
