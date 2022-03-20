675b8897fac4d6b1b3a38f307ca7c94256f56383
kaiyu li <krisfk@gmail.com>
2022-03-21 02:42:59 +0800

    .

--- wp-content/themes/sweetdate/page-member-detail-info.php
+++ wp-content/themes/sweetdate/page-member-detail-info.php
@@ -66,7 +66,8 @@ get_header();
         </form>
 
         <h2 class="text-center mb-3">我的資料</h2>
-        <div>請閣下細心提供資料, 愈精準, 找到合適伴侶的機率愈高!
+        <div class="pink">請閣下細心提供資料, 愈精準, 找到合適伴侶的機率愈高!
+
         </div>
 
         <form class="member-further-info-form">
