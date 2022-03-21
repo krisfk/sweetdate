039ed99f53b61c9daf616516fa31e1f2b0793f34
kaiyu li <krisfk@gmail.com>
2022-03-21 21:37:25 +0800

    .

--- wp-content/themes/sweetdate/page-member-detail-info.php
+++ wp-content/themes/sweetdate/page-member-detail-info.php
@@ -117,7 +117,7 @@ get_header();
                 </tr>
                 <tr>
                     <td>
-                        <label for="first-name">英文名* (e.g.Jacky)：</label>
+                        <label for="first-name">英文名* (e.g.Jacky)：*</label>
                     </td>
                     <td>
 
@@ -125,7 +125,7 @@ get_header();
                     </td>
                 </tr>
                 <tr>
-                    <td><label for="last-name">姓：</label></td>
+                    <td><label for="last-name">姓：*</label></td>
                     <td>
                         <input id="last-name" class="form-control" name="last-name" type="text">
                     </td>
@@ -133,10 +133,86 @@ get_header();
 
                 <tr>
                     <td>
-                        <label for="age">年齡：</label>
+                        <label for="age">年齡：*</label>
                     </td>
                     <td>
-                        <input id="age" class="form-control" name="age" type="text">
+                        <select id="age" name="age" class="age" aria-label="">
+                            <option value="" selected="">選擇</option>
+                            <option value="20-29">20-29</option>
+                            <option value="30-34">30-34</option>
+                            <option value="35-39">35-39</option>
+                            <option value="40或以上">40或以上</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="education-level">教育程度：*</label>
+
+
+                    </td>
+                    <td>
+
+                        <select id="education-level" name="education-level" class="education-level" aria-label="">
+                            <option value="" selected="">選擇</option>
+                            <option value="小學程度">小學程度</option>
+                            <option value="中學程度">中學程度</option>
+                            <option value="大學程度">大學程度</option>
+                            <option value="碩士">碩士</option>
+                            <option value="博士">博士</option>
+
+                        </select>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="occupation">職業</label>
+
+                    </td>
+                    <td>
+
+                        <select id="occupation" name="occupation" class="form-control">
+                            <option value="教師">教師</option>
+                            <option value="研究員">研究員</option>
+                            <option value="編輯">編輯</option>
+                            <option value="翻譯">翻譯</option>
+                            <option value="記者">記者</option>
+                            <option value="作家">作家</option>
+                            <option value="文化/美術/文藝工作者">文化/美術/文藝工作者</option>
+                            <option value="教練">教練</option>
+                            <option value="運動員">運動員</option>
+                            <option value="工程師">工程師</option>
+                            <option value="技術員">技術員</option>
+                            <option value="會計">會計</option>
+                            <option value="企業管理人員">企業管理人員</option>
+                            <option value="廚師">廚師</option>
+                            <option value="農藝師">農藝師</option>
+                            <option value="工人">工人</option>
+                            <option value="農民">農民</option>
+                            <option value="牧民">牧民</option>
+                            <option value="漁民">漁民</option>
+                            <option value="醫生">醫生</option>
+                            <option value="藥劑師">藥劑師</option>
+                            <option value="護士">護士</option>
+                            <option value="機長">機長</option>
+                            <option value="飛行員">飛行員</option>
+                            <option value="船長">船長</option>
+                            <option value="乘務員">乘務員</option>
+                            <option value="公務員">公務員</option>
+                            <option value="服務員">服務員</option>
+                            <option value="律師">律師</option>
+                            <option value="商人">商人</option>
+                            <option value="宗教職業者">宗教職業者</option>
+                            <option value="家庭婦女">家庭婦女</option>
+                            <option value="退休">退休</option>
+                            <option value="無業">無業</option>
+                            <option value="學生">學生</option>
+                            <option value="兒童">兒童</option>
+                            <option value="職員">職員</option>
+                            <option value="法律工作者">法律工作者</option>
+                            <option value="教授">教授</option>
+                        </select>
                     </td>
                 </tr>
                 <tr>
