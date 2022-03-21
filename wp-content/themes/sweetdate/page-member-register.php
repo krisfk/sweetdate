cb0183afdac72e7ba57b26dd3cbb0694b209c9e7
kaiyu li <krisfk@gmail.com>
2022-03-22 05:04:13 +0800

    .

--- wp-content/themes/sweetdate/page-member-register.php
+++ wp-content/themes/sweetdate/page-member-register.php
@@ -29,21 +29,21 @@ get_header();
         <form action="" class="login-form">
             <table>
                 <tr>
-                    <td class="text-end"><label for="login-email"> 登記電郵：</label></td>
+                    <td class="text-end"><label for="login-email"> 登記電郵：*</label></td>
                     <td><input type="text" class="form-control" id="login-email" name="login-email"></td>
                 </tr>
                 <tr>
-                    <td class="text-end"><label for="login-password">登入密碼：</label> </td>
+                    <td class="text-end"><label for="login-password">登入密碼：*</label> </td>
                     <td><input type="password" class="form-control" id="login-password" name="login-password"></td>
                 </tr>
                 <tr>
-                    <td class="text-end"><label for="login-password-again">再次輸入登入密碼：</label> </td>
+                    <td class="text-end"><label for="login-password-again">再次輸入登入密碼：*</label> </td>
                     <td><input type="password" class="form-control" id="login-password" name="login-password-again">
                     </td>
                 </tr>
 
                 <tr>
-                    <td class="text-end">姓別：</td>
+                    <td class="text-end">姓別：*</td>
                     <td>
                         <label class="me-2"><input class="me-1" type="radio" name="gender" value="男">男</label>
                         <label class="me-2"><input class="me-1" type="radio" name="gender" value="女">女</label>
@@ -64,14 +64,14 @@ get_header();
                     </td>
                 </tr>
                 <tr>
-                    <td class="text-end"><label for="tel">聯絡電話：</label></td>
+                    <td class="text-end"><label for="tel">聯絡電話：*</label></td>
                     <td><input type="text" class="form-control" id="tel" name="tel"></td>
                 </tr>
 
                 <tr>
 
                     <td class=" text-end">
-                        <label for="msg-content">最喜歡吃的食物：</label>
+                        <label for="msg-content">最喜歡吃的食物：*</label>
                     </td>
                     <td>
                         <select id="like-food" name="like-food" class="form-select" aria-label="">
@@ -87,7 +87,7 @@ get_header();
                 <tr>
 
                     <td class=" text-end">
-                        <label for="msg-content">最不喜歡吃的食物：</label>
+                        <label for="msg-content">最不喜歡吃的食物：*</label>
                     </td>
                     <td>
 
