662b35cf817588f957b3c180a5d793e261889f40
kaiyu li <krisfk@gmail.com>
2022-03-26 04:53:07 +0800

    .

--- wp-content/themes/sweetdate/page-test.php
+++ wp-content/themes/sweetdate/page-test.php
@@ -1,15 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+result
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+if($_POST){
+    require_once('./stripe/init.php');
 
-<body>
-    test
-</body>
+    \Stripe\Stripe::setApiKey('sk_test_g9irmT4kYqGJ7bZi4Z6bw4j100ZM9jPv5o');
 
-</html>
+    $token = $_POST['stripeToken'];
+
+    $IMEI = $_POST['ImeiNum'];
+    // echo $_POST['name'];
+    try {
+        $charge = \Stripe\Charge::create(array(
+          "amount" => 1000,
+          "currency" => "usd",
+          "source" => $token,
+          "description" => $email,
+          "metadata" => array("IMEI" => $IMEI))
+        );
+
+        print_r($charge);
+        // echo $charge->id;
+    }catch(\Stripe\Error\Card $e){
+        echo $e->getMessage();
+    }
+
+    
+}
+?>
--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -361,34 +361,7 @@ get_header();
                 </form>
             </div>
 
-            <?php if($_POST){
-                echo 999;
-    require_once('http://64.227.13.14/sweetdate/stripe-test/stripe/init.php');
 
-    \Stripe\Stripe::setApiKey('sk_test_g9irmT4kYqGJ7bZi4Z6bw4j100ZM9jPv5o');
-
-    $token = $_POST['stripeToken'];
-
-    $IMEI = $_POST['ImeiNum'];
-    // echo $_POST['name'];
-    try {
-        $charge = \Stripe\Charge::create(array(
-          "amount" => 1000,
-          "currency" => "usd",
-          "source" => $token,
-          "description" => $email,
-          "metadata" => array("IMEI" => $IMEI))
-        );
-
-        print_r($charge);
-        // echo $charge->id;
-    }catch(\Stripe\Error\Card $e){
-        echo $e->getMessage();
-    }
-
-    
-}
-    ?>
             <div></div>
 
         </div>
