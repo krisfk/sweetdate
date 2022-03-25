d15b2344e97ec692739899920f5710bf8f981be6
kaiyu li <krisfk@gmail.com>
2022-03-26 04:49:16 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -362,6 +362,7 @@ get_header();
             </div>
 
             <?php if($_POST){
+                echo 999;
     require_once('http://64.227.13.14/sweetdate/stripe-test/stripe/init.php');
 
     \Stripe\Stripe::setApiKey('sk_test_g9irmT4kYqGJ7bZi4Z6bw4j100ZM9jPv5o');
