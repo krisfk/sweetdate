6ae65d2da21b150a40c7948c985da2cddb6beceb
kaiyu li <krisfk@gmail.com>
2022-03-26 04:45:13 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -361,6 +361,34 @@ get_header();
                 </form>
             </div>
 
+            <?php if($_POST){
+    require_once('./stripe/init.php');
+
+    \Stripe\Stripe::setApiKey('sk_test_g9irmT4kYqGJ7bZi4Z6bw4j100ZM9jPv5o');
+
+    $token = $_POST['stripeToken'];
+
+    $IMEI = $_POST['ImeiNum'];
+    // echo $_POST['name'];
+    try {
+        $charge = \Stripe\Charge::create(array(
+          "amount" => 1000,
+          "currency" => "usd",
+          "source" => $token,
+          "description" => $email,
+          "metadata" => array("IMEI" => $IMEI))
+        );
+
+        print_r($charge);
+        // echo $charge->id;
+    }catch(\Stripe\Error\Card $e){
+        echo $e->getMessage();
+    }
+
+    
+}
+    ?>
+            <div></div>
 
         </div>
 
