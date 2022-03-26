459420afc52468dfc601df6b60dbef110df5a2be
kaiyu li <krisfk@gmail.com>
2022-03-26 17:58:35 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -418,7 +418,7 @@ if($_POST['form-type']=='reg')
           "amount" => get_field('price')*100,
           "currency" => "hkd",
           "source" => $token,
-          "description" => $_POST['first-name'].' '.$_POST('register-email').' '.$_POST('tel'),
+          "description" => get_field('register-email'),
           "metadata" => array("IMEI" => $IMEI))
           
 
