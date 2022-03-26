c5f6a0c9fac59728fcd0821f294632cc80580b1a
kaiyu li <krisfk@gmail.com>
2022-03-27 02:40:23 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -172,8 +172,7 @@ if($_POST['form-type']=='reg' && $_SESSION['paid'])
                 if($_GET['person']==2)
                 {
                     ?>
-                <span class="price me-2">二人同行優惠價 <br>
-                    <span class="line-through">$<?php echo get_field('price');?></span>
+                <span class="price me-2">二人同行優惠價: <span class="line-through">$<?php echo get_field('price');?></span>
                     $<?php echo get_field('two_people_discount_price');?>/位</span>
 
                 <?php
