a12fc8778f1174e3d09f11dee4bd9a2149d33792
kaiyu li <krisfk@gmail.com>
2022-03-27 02:06:02 +0800

    .

--- wp-content/themes/sweetdate/page-home.php
+++ wp-content/themes/sweetdate/page-home.php
@@ -256,8 +256,8 @@ get_header();
                                 一人參加
                             </a>
                             <a class="entry-person-btn" href="<?php echo get_permalink();?>">
-                                $<?php echo get_field('price');?> <br>
-                                二人同行 (附優惠價) </a>
+                                (附優惠價)$<?php echo get_field('price');?> <br>
+                                二人同行</a>
                         </div>
 
                         <div class="gender-div male">
