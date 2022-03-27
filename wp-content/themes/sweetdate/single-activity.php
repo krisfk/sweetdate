0221e9afc4d7bf2ec45578cb20ff12efd9badcc3
kaiyu li <krisfk@gmail.com>
2022-03-28 00:32:38 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -192,7 +192,7 @@ if($_POST['form-type']=='reg' && $_SESSION['paid'])
                 <span class="price me-2">非指定日子優惠價: <span class="line-through">$<?php echo get_field('price');?></span>
                     $<?php echo get_field('price')-100;?>/位</span>
             </div>
-
+            <!--  -->
             <div class="one-ppl-no-discount">
                 <span class="price me-2">$<?php echo get_field('price');?>/位</span>
             </div>
