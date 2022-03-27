7eff94001d3d5295417e7588e35a678e2a9ca96d
kaiyu li <krisfk@gmail.com>
2022-03-27 23:36:12 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -191,7 +191,7 @@ if($_POST['form-type']=='reg' && $_SESSION['paid'])
 
 
 
-            <div class="">
+            <div class="mb-5">
                 <div class="checkbox-div"><input type="checkbox" class="form-check-input me-2" id="" name=""
                         value=""><label for="">接受指定日子以外的日子(其他日子，如接受有-$100
                         優惠)</label>
