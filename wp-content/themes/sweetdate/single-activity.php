44683d5735a13ee082eac2e4a3a759ce1930caa8
kaiyu li <krisfk@gmail.com>
2022-03-30 05:54:54 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -796,7 +796,8 @@ if($_POST['form-type']=='reg')
     
     // echo $_POST['name'];
     // echo $charge_final_price;
-    Number($_POST['final-price']*100);
+    // Number($_POST['final-price']*100)
+    echo $_POST['final-price']*100;
     try {
         $charge = \Stripe\Charge::create(array(
         //   "amount" => $_POST('final-price')*100,
