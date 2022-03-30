91672dcb2855003c4ff05a2387d9499b8c2d7e97
kaiyu li <krisfk@gmail.com>
2022-03-30 23:41:55 +0800

    .

--- wp-content/themes/sweetdate/functions.php
+++ wp-content/themes/sweetdate/functions.php
@@ -15,9 +15,9 @@
 // define( 'WP_DEBUG', true );
 
 // // Enable Debug logging to the /wp-content/debug.log file
-define( 'WP_DEBUG_LOG', true );
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+// define( 'WP_DEBUG_LOG', true );
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
 session_start();
 
