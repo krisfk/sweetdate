14432c802845030d87aed27718f264197a11bea2
kaiyu li <krisfk@gmail.com>
2022-04-03 04:34:11 +0800

    .

--- wp-content/themes/sweetdate/single.php
+++ wp-content/themes/sweetdate/single.php
@@ -1,15 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+get_header();
 
-<body>
-    d
-</body>
+?>
+<div class="container mt-3">
 
-</html>
+    <div class="position-relative">
+        <h2 class="title-type-pink-line"><?php echo get_the_title();?></h2>
+
+        <div class="text-center">
+            <div class="title-pink-line"></div>
+        </div>
+    </div>
+
+    <div class="main-content mt-4">
+
+        <div class="text-center">
+            <img src="<?php echo wp_get_attachment_image_url(get_field('activity_main_image'),'full');?>" alt="">
+        </div>
+        <br>
+
+
+
+
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+<?php
+/* Start the Loop */
+// while ( have_posts() ) :
+// 	the_post();
+// 	get_template_part( 'template-parts/content/content-page' );
+
+// 	// If comments are open or there is at least one comment, load up the comment template.
+// 	if ( comments_open() || get_comments_number() ) {
+// 		comments_template();
+// 	}
+// endwhile; // End of the loop.
+
+get_footer();
