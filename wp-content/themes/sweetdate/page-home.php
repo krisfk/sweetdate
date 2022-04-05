ed7050f19506b4e67ea8cfb006d9db728b7b6cea
kaiyu li <krisfk@gmail.com>
2022-04-06 04:47:08 +0800

    .

--- wp-content/themes/sweetdate/page-home.php
+++ wp-content/themes/sweetdate/page-home.php
@@ -362,29 +362,7 @@ get_header();
                         </div>
 
 
-                        <!-- <div class="gender-div female">
-                            <table>
-                                <tr>
-                                    <td class="gender-icon-td">
-                                        <img src="<?php echo get_template_directory_uri()?>/assets/images/f-icon.png"
-                                            alt="">
 
-
-                                    </td>
-                                    <td class="">
-                                        <div class="row">
-
-                                            <div class="col-6">名額 : <?php echo get_field('female_quota');?>人
-                                                (<?php echo get_field('female_age_range');?>歲)</div>
-                                            <div class="col-6">餘額 : <?php echo get_field('female_remain_quota');?>人
-                                            </div>
-                                            <div class="col-12 mt-1"><?php echo get_field('female_requirement');?></div>
-                                        </div>
-                                    </td>
-                                </tr>
-
-                            </table>
-                        </div> -->
 
 
                     </div>
