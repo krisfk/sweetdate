b1401a9a4ed597c2171c2a957157d721a99f9732
kaiyu li <krisfk@gmail.com>
2022-04-06 04:50:07 +0800

    .

--- wp-content/themes/sweetdate/page-new-activities.php
+++ wp-content/themes/sweetdate/page-new-activities.php
@@ -138,11 +138,11 @@ $the_query->the_post();
 
                         <div class="gender-div male row align-items-center gx-0 mt-3">
 
-                            <div class="col-3 text-center pt-3 pb-3"> <img
+                            <div class="col-2 text-center pt-3 pb-3"> <img
                                     src="<?php echo get_template_directory_uri()?>/assets/images/m-icon.png" alt="">
                             </div>
 
-                            <div class=" col-9 pt-3 pe-2 pb-3">
+                            <div class=" col-10 pt-3 pe-2 pb-3">
                                 <div class="row">
 
                                     <div class="col-6">名額 : <?php echo get_field('male_quota');?>人
@@ -158,11 +158,11 @@ $the_query->the_post();
 
                         <div class="gender-div female row align-items-center gx-0 mt-1">
 
-                            <div class="col-3 text-center pt-3 pb-3"> <img
+                            <div class="col-2 text-center pt-3 pb-3"> <img
                                     src="<?php echo get_template_directory_uri()?>/assets/images/f-icon.png" alt="">
                             </div>
 
-                            <div class="col-9 pt-3 pe-2 pb-3">
+                            <div class="col-10 pt-3 pe-2 pb-3">
                                 <div class="row">
 
                                     <div class="col-6">名額 : <?php echo get_field('female_quota');?>人
