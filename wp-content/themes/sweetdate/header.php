51f909af8d865ba2a0cdc95e4bf63c261348e836
kaiyu li <krisfk@gmail.com>
2022-04-05 22:59:13 +0800

    .

--- wp-content/themes/sweetdate/header.php
+++ wp-content/themes/sweetdate/header.php
@@ -100,7 +100,7 @@ session_start();
                 </a>
 
 
-                <ul class=" top-menu-ul col-8">
+                <ul class=" top-menu-ul">
 
                     <li class="mobile-logo-li"> <a href="<?php echo get_site_url();?>" class="logo-a col-2">
                             <img src="<?php echo get_template_directory_uri()?>/assets/images/sd-logo-new.jpg" alt="">
