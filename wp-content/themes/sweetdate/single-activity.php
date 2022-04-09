64f61129dd47bef7d472cc1316f1ee39ffc66af1
kaiyu li <krisfk@gmail.com>
2022-04-09 18:01:54 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -1207,7 +1207,7 @@ if($_POST['payment-method']=='轉數快/Payme付款')
             <h4 class="pink">
                 我們已收到你的報名！會盡快聯絡您！謝謝！ <br>
             </h4>
-            <div>因為您選擇了轉數快/Payme付款<br>麻煩從以下payme qrcode 或fps id 付款後聯絡我們，謝謝！</div>
+            <div class="mt-4">因為您選擇了轉數快/Payme付款<br>麻煩從以下payme qrcode 或fps id 付款後聯絡我們，謝謝！</div>
             <div>
                 <div>FPS ID: 7420482</div> <br>
                 <img src="https://sweetdate.com.hk/wp-content/uploads/2022/04/payme-code.jpeg" alt="">
