3b596f6eb53cd6a87692a934dd5467fd3aa7e497
kaiyu li <krisfk@gmail.com>
2022-04-10 02:09:11 +0800

    .

--- wp-content/themes/sweetdate/single-activity.php
+++ wp-content/themes/sweetdate/single-activity.php
@@ -1103,7 +1103,7 @@ if($_POST['form-type']=='reg')
                         'date_of_purchase' => date('d-m-y h:i:s'),
                         'non_member_info_url' => $post_id,
                         'random_date_select' => $random_date_select,
-                        'join_charm_workshop' => $_POST['join-charm-workshop'] ? '1' :'0'
+                        // 'join_charm_workshop' => $_POST['join-charm-workshop'] ? '1' :'0'
 
                     );
                     add_row('applied_non_member', $row,$current_post_id);
@@ -1123,7 +1123,7 @@ if($_POST['form-type']=='reg')
                         'date_of_purchase' => date('d-m-y h:i:s'),
                         'member_info_url' => $_SESSION['mid'],
                         'random_date_select' => $random_date_select,
-                        'join_charm_workshop' => $_POST['join-charm-workshop'] ? '1' :'0'
+                        // 'join_charm_workshop' => $_POST['join-charm-workshop'] ? '1' :'0'
 
                         
                     );
