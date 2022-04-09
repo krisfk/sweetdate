f1546d71a80372b2d2af25866c4fe77c622ccd6d
kaiyu li <krisfk@gmail.com>
2022-04-09 15:00:56 +0800

    .

--- wp-content/themes/sweetdate/page-test.php
+++ wp-content/themes/sweetdate/page-test.php
@@ -1,29 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-// require_once('./stripe-test/init.php');
-require_once('./stripe-test/stripe/init.php');
+<body>
+    test
+</body>
 
-    \Stripe\Stripe::setApiKey('sk_test_g9irmT4kYqGJ7bZi4Z6bw4j100ZM9jPv5o');
-
-    $token = $_POST['stripeToken'];
-
-    $IMEI = $_POST['ImeiNum'];
-    // echo $_POST['name'];
-    try {
-        $charge = \Stripe\Charge::create(array(
-          "amount" => 1000,
-          "currency" => "usd",
-          "source" => $token,
-          "description" => $email,
-          "metadata" => array("IMEI" => $IMEI))
-        );
-
-        print_r($charge);
-        // echo $charge->id;
-    }catch(\Stripe\Error\Card $e){
-        echo $e->getMessage();
-    }
-
-
- ?>
+</html>
