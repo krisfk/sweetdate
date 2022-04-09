2e2f7f959a348c926bff3afba10cb34fa0e5f88a
kaiyu li <krisfk@gmail.com>
2022-04-09 18:33:36 +0800

    .

--- stripe-test/charge.php
+++ stripe-test/charge.php
@@ -5,17 +5,18 @@ require_once('./stripe/init.php');
 
     \Stripe\Stripe::setApiKey('sk_test_51Hc7UyK6XoTSXbpZGRS7d11xxvWwPrRlPwyYvMcQ6LSXQVEw5k68nUhs3s7ISeaFd7RPAeM0yDn8adhEmyIS9sO900enLknpWs');
 
-    $token = $_POST['stripeToken'];
+    // $token = $_POST['stripeToken'];
 
     $IMEI = $_POST['ImeiNum'];
     // echo $_POST['name'];
     try {
         $charge = \Stripe\Charge::create(array(
           "amount" => 1000,
-          "currency" => "usd",
-          "source" => $token,
-          "description" => $email,
-          "metadata" => array("IMEI" => $IMEI))
+          "currency" => "usd"
+        //   "source" => $token,
+        //   "description" => $email,
+        //   "metadata" => array("IMEI" => $IMEI))
+        )
         );
 
         print_r($charge);
