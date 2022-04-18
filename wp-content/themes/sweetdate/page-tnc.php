862156dbbc023194be537ae35b0f8c4760b3ba2e
kaiyu li <krisfk@gmail.com>
2022-04-18 22:49:25 +0800

    .

---
+++ wp-content/themes/sweetdate/page-tnc.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+<div class="container mt-3">
+
+    <div class="position-relative">
+        <h2 class="title-type-pink-line">關於我們</h2>
+
+        <div class="text-center">
+            <div class="title-pink-line"></div>
+        </div>
+    </div>
+
+    <div class="main-content mt-4">
+
+
+        <?php echo get_the_content();?>
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+<?php
+/* Start the Loop */
+// while ( have_posts() ) :
+// 	the_post();
+// 	get_template_part( 'template-parts/content/content-page' );
+
+// 	// If comments are open or there is at least one comment, load up the comment template.
+// 	if ( comments_open() || get_comments_number() ) {
+// 		comments_template();
+// 	}
+// endwhile; // End of the loop.
+
+get_footer();
