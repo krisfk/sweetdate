5e28a981e12b82d8780dd061fbcf2b9a3b4667ee
kaiyu li <krisfk@gmail.com>
2022-05-17 05:10:24 +0800

    .

--- wp-content/themes/sweetdate/page-home.php
+++ wp-content/themes/sweetdate/page-home.php
@@ -61,47 +61,11 @@ if( have_rows('carousel') ){
                             ?>
 
                 <div class="banner-content-div position-absolute top-0 start-0 w-100 h-100">
-                    <!-- <iframe class="video-iframe"
+                    <iframe class="video-iframe"
                         src="https://www.youtube.com/embed/A7ZUt_2jrxg?mute=1&autoplay=1&rel=0&playsinline=1"
                         title="YouTube video player" frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe> -->
-                    <div id="player"></div>
-
-                    <script>
-                    // 2. This code loads the IFrame Player API code asynchronously.
-                    var tag = document.createElement('script');
-
-                    tag.src = "https://www.youtube.com/iframe_api";
-                    var firstScriptTag = document.getElementsByTagName('script')[0];
-                    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-                    // 3. This function creates an <iframe> (and YouTube player)
-                    //    after the API code downloads.
-                    var player;
-
-                    function onYouTubeIframeAPIReady() {
-                        player = new YT.Player('player', {
-                            width: '100%',
-                            videoId: 'A7ZUt_2jrxg',
-                            playerVars: {
-                                'autoplay': 1,
-                                'playsinline': 1,
-                                'mute': 1
-                            },
-                            events: {
-                                'onReady': onPlayerReady
-                            }
-                        });
-                    }
-
-                    // 4. The API will call this function when the video player is ready.
-                    function onPlayerReady(event) {
-                        event.target.mute();
-                        event.target.playVideo();
-                    }
-                    </script>
-
+                        allowfullscreen></iframe>
                 </div>
 
                 <?php
