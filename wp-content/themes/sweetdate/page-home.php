228df33e098d61da60b0522814a31109fb973adb
kaiyu li <krisfk@gmail.com>
2022-05-17 05:16:37 +0800

    .

--- wp-content/themes/sweetdate/page-home.php
+++ wp-content/themes/sweetdate/page-home.php
@@ -63,18 +63,16 @@ if( have_rows('carousel') ){
 
 
                 <div class="banner-content-div position-absolute top-0 start-0 w-100 h-100">
-                    <div id="player"></div>
+                    <div id="player" class="video-player"></div>
 
                     <script>
-                    // 2. This code loads the IFrame Player API code asynchronously.
                     var tag = document.createElement('script');
 
                     tag.src = "https://www.youtube.com/iframe_api";
                     var firstScriptTag = document.getElementsByTagName('script')[0];
                     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-                    // 3. This function creates an <iframe> (and YouTube player)
-                    //    after the API code downloads.
+
                     var player;
 
                     function onYouTubeIframeAPIReady() {
